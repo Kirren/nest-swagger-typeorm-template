99aa9ee54792124733778dc57c3cc5af1732b217
Kirren <kirren@live.com>
2019-09-19 14:10:03 +0300

    Add Swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 (async () => {
     const app = await NestFactory.create(AppModule);
+
+    const options = new DocumentBuilder()
+        .setTitle("Requesters API")
+        .setDescription("Requesters API description")
+        .setVersion("1.0")
+        .setBasePath("/api")
+        .addTag("requesters")
+        .setSchemes("https")
+        .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup("api", app, document);
 
     await app.listen(3000);
 })();
